b05a6687a0d07821068eb523ae0bc0243ec7b2cd
Veres-Lakos Zsombor <vl.zsombor@gmail.com>
2021-05-23 18:18:28 +0200

    working correlation

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import csv
-import codecs
 import numpy as np
+import codecs
 
 
-with open('gyorsabb2299.csv') as csv_file:
-    csv_reader = csv.reader(csv_file)
-    line_count = 0
+
+# csvReader = csv.reader(codecs.open('commaMerged2.csv', 'rU', 'utf-16'))
+
+
+with open('mergedSorted.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=';')
     data = list(csv_reader)
 
+arr = np.array(data)
+print(arr[:,0])
 
-
-
-arr = np.array(data,dtype=np.str)
-
-print(arr[:,2:6].astype(np.int).mean(axis=1))
-print(arr[:,6:10].astype(np.int).mean(axis=1))
-
-print(np.corrcoef(arr[:,2:6].astype(np.int).mean(axis=1),arr[:,6:10].astype(np.int).mean(axis=1)))
+subArr = arr[0:1000]
+print(np.corrcoef(subArr[:,2:6].astype(np.int).mean(axis=1),subArr[:,6:10].astype(np.int).mean(axis=1)))
